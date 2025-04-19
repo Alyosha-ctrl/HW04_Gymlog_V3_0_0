8da8b6127180cba6203b02bad4443cc7a3665efd
Alyosha-ctrl <alexeibere@gmail.com>
2025-04-18 17:16:30 -0700

    Made the text at the top scrollable and updated the click method for the button to get and log the information from the display.

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hw04_gymlog_v300;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,9 +14,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.hw04_gymlog_v300.databinding.ActivityMainBinding;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "DAC_GYMLOG";
+
     ActivityMainBinding binding;
+
+    String excercise = "";
+    double weight = 0.0;
+    int reps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +32,37 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                updateDisplay();
                 Toast.makeText(MainActivity.this, "It Worked", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void updateDisplay(){
+        getInformationFromDisplay();
+        String currentInfo = binding.logDisplayTextView.getText().toString();
+        String newDisplay = String.format(Locale.US, "Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s", excercise, weight, reps, currentInfo);
+        binding.logDisplayTextView.setText(newDisplay);
+    }
+
+    private void getInformationFromDisplay(){
+        excercise = binding.ExerciseInputEditText.getText().toString();
+        try {
+            weight = Double.parseDouble(binding.WeightInputEditText.getText().toString());
+        }
+        catch (NumberFormatException e){
+            Log.d(TAG, "Error reading vale from WeightInputEditText");
+        }
+        try {
+            reps = Integer.parseInt(binding.RepsInputEditText.getText().toString());
+        }
+        catch (NumberFormatException e){
+            Log.d(TAG, "Error reading vale from RepsInputEditText");
+        }
     }
 }
