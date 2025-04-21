34ae0a8e43276c11298b57321534a30d3bc8c7fe
Alyosha-ctrl <alexeibere@gmail.com>
2025-04-20 23:15:24 -0700

    Finished video 4, added a insertGymLogRecord to main and encountered an error.

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hw04_gymlog_v300.Database.GymLogRepository;
+import com.example.hw04_gymlog_v300.Database.entities.GymLog;
 import com.example.hw04_gymlog_v300.databinding.ActivityMainBinding;
 
 import java.util.Locale;
@@ -14,11 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "DAC_GYMLOG";
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     String exercise = "";
     double weight = 0.0;
     int reps = 0;
+
+    private GymLogRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +31,24 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        repository = new GymLogRepository(getApplication());
+
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
 
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getInformationFromDisplay();
+                insertGymLogRecord();
                 updateDisplay();
                 Toast.makeText(MainActivity.this, "It Worked", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void insertGymLogRecord(){
+        GymLog log = new GymLog(exercise, weight, reps);
+        repository.insertGymLog(log);
     }
 
     /**
@@ -42,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
      * and then adds the stuff to the screen.
      */
     private void updateDisplay(){
-        getInformationFromDisplay();
         String currentInfo = binding.logDisplayTextView.getText().toString();
         String newDisplay = String.format(Locale.US, "Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s", exercise, weight, reps, currentInfo);
         binding.logDisplayTextView.setText(newDisplay);
